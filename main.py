8eced4176f74b5743130f5f20bc2bb9e55280ae0
AkillesU <a.rechardt@gmail.com>
2024-12-06 10:33:27 +0000

    added main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import torch
+from cornet import cornet_s
+from torchsummary import summary
+
+
+def main():
+
+    # Initialize the model
+    model = cornet_s(pretrained=True)
+
+
+    
+
+
+if __name__ == "__main__":
+    main()
